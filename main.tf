49aa21ce8f7bd95c3fe1f0cd6b818ffc5f69cff4
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-22 23:46:40 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "app_instance" {
+resource "aws_instance" "jenkins_agent" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
   key_name      = "dschool"
 
-  vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
+  vpc_security_group_ids = [
+    "sg-0727ee3d6745a3427"
+  ]
 
   tags = {
-    Name = "certask-app-instance"
+    Name = "jenkins-agent"
   }
 }
