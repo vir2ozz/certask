846b95290635b1089e99f557006f5e8183e5a525
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-23 02:14:16 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "default"
+  profile                 = "devops-student"
 }
 
 resource "aws_instance" "java_builder" {
