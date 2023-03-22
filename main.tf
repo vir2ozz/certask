4b1f5c048dabd2317928c972227c223563089b0d
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-22 13:16:24 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,23 @@
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
   access_key = "AKIA5M4V4GFC47QOD6VK"
-  secret_key = "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"
+  secret_access_key = "XrwB+wFM2qoe4cLoXcFPwaevnxQTKTEqCnChBBFV"
 }
 
 resource "aws_instance" "app" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
+  key_name      = "jenkins-ec2-keypair"
 
-  key_name               = "jenkins-key"
-  associate_public_ip_address = true
-  vpc_security_group_ids = "sg-0727ee3d6745a3427"
+  vpc_security_group_ids = [
+    "sg-0727ee3d6745a3427",
+  ]
 
   tags = {
-    Name = "jenkins-agent"
+    Name = "app-instance"
   }
+}
+
+output "app_instance_ip" {
+  value = aws_instance.app.public_ip
 }
