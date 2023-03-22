11f56616fdf8de575307ddfe6d228c29f33b871d
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-22 15:10:34 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,16 @@
 provider "aws" {
-  region              = "us-east-1"
-  access_key          = "AKIA5M4V4GFC47QOD6VK"
-  secret_key          = "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"
+  region  = "us-east-1"
+  profile = "devops"
 }
 
 resource "aws_instance" "app" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
-  key_name      = "jenkins-ec2-keypair"
+  key_name      = "dschool"
 
-  vpc_security_group_ids = [
-    "sg-0727ee3d6745a3427",
-  ]
+  vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
 
   tags = {
-    Name = "app-instance"
+    Name = "jenkins-app-instance"
   }
-}
-
-output "app_instance_ip" {
-  value = aws_instance.app.public_ip
 }
