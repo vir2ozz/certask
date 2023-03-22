dba44ee6d6dace623069bef0920794d54f5f7b2e
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-22 12:50:27 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 provider "aws" {
-  region = var.aws_region
+  region     = "us-east-1"
+  access_key = "AKIA5M4V4GFC47QOD6VK"
+  secret_key = "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"
 }
 
-resource "aws_instance" "stage" {
-  ami           = var.ami
-  instance_type = var.instance_type
+resource "aws_instance" "app" {
+  ami           = "ami-09cd747c78a9add63"
+  instance_type = "t2.small"
+
+  key_name               = "jenkins-key"
+  associate_public_ip_address = true
+  vpc_security_group_ids = "sg-0727ee3d6745a3427"
 
   tags = {
-    Name = "stage"
+    Name = "jenkins-agent"
   }
 }
