708411034de33736f4c7dcd968f3bef49af12258
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-22 16:15:29 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  region  = "us-east-1"
-  profile = "devops"
+  region     = "us-east-1"
+  access_key = "AKIA5M4V4GFC2ILCVWOT"
+  secret_key = "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"
 }
 
-resource "aws_instance" "app" {
+resource "aws_instance" "app_instance" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
   key_name      = "dschool"
@@ -11,6 +12,6 @@ resource "aws_instance" "app" {
   vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
 
   tags = {
-    Name = "jenkins-app-instance"
+    Name = "certask-app-instance"
   }
 }
