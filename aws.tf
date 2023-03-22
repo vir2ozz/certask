7be710a7a10faf5b67b624cc7cb16e2ffd446d9b
vir2ozz <98392003+vir2ozz@users.noreply.github.com>
2023-03-22 11:27:31 +0600

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,1 +1,35 @@
+provider "aws" {
+  access_key = "<your_access_key>"
+  secret_key = "<your_secret_key>"
+  region     = "us-east-1"
+}
 
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  count         = 2
+
+  tags = {
+    Name = "ExampleInstance-${count.index + 1}"
+  }
+
+  provisioner "file" {
+    source      = "docker-compose.yml"
+    destination = "/home/ubuntu/docker-compose.yml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y docker.io",
+      "sudo docker-compose up -d",
+    ]
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("<path_to_your_private_key>")
+    host = self.public_ip
+  }
+}
