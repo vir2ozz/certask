38ceed89fa741289591b7900e7926a7940fd00be
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-23 20:36:39 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,34 +2,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "instance1" {
+resource "aws_instance" "jenkins_agent" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
 
-  key_name          = "dschool"
-  security_group_id = "sg-0727ee3d6745a3427"
+  key_name        = "dschool"
+  security_groups = ["launch-wizard-2"]
 
   tags = {
-    Name = "Instance1"
+    Name = "jenkins_agent"
   }
 }
 
-resource "aws_instance" "instance2" {
+resource "aws_instance" "docker_host" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
 
-  key_name          = "dschool"
-  security_group_id = "sg-0727ee3d6745a3427"
+  key_name        = "dschool"
+  security_groups = ["launch-wizard-2"]
 
   tags = {
-    Name = "Instance2"
+    Name = "docker_host"
   }
 }
 
-output "instance1_public_ip" {
-  value = aws_instance.instance1.public_ip
+output "jenkins_agent_public_ip" {
+  value = aws_instance.jenkins_agent.public_ip
 }
 
-output "instance2_public_ip" {
-  value = aws_instance.instance2.public_ip
+output "docker_host_public_ip" {
+  value = aws_instance.docker_host.public_ip
 }
