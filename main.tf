d17d54d478d2c42214c85b283e317872888ed26d
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-23 12:41:11 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,32 +2,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "instance_1" {
+resource "aws_instance" "instance1" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
-  key_name      = "dschool"
-  vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
+
+  key_name          = "dschool"
+  security_group_id = "sg-0727ee3d6745a3427"
 
   tags = {
-    Name = "jenkins-instance-1"
+    Name = "Instance1"
   }
 }
 
-resource "aws_instance" "instance_2" {
+resource "aws_instance" "instance2" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
-  key_name      = "dschool"
-  vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
+
+  key_name          = "dschool"
+  security_group_id = "sg-0727ee3d6745a3427"
 
   tags = {
-    Name = "jenkins-instance-2"
+    Name = "Instance2"
   }
 }
 
-output "instance_1_ip" {
-  value = aws_instance.instance_1.public_ip
+output "instance1_public_ip" {
+  value = aws_instance.instance1.public_ip
 }
 
-output "instance_2_ip" {
-  value = aws_instance.instance_2.public_ip
+output "instance2_public_ip" {
+  value = aws_instance.instance2.public_ip
 }
