594ffa4daa82afad4366a601e3c253d3a112fb61
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-23 12:12:18 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,38 +2,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "app_instance" {
+resource "aws_instance" "instance_1" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
   key_name      = "dschool"
-
-  vpc_security_group_ids = [
-    "sg-0727ee3d6745a3427",
-  ]
+  vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
 
   tags = {
-    Name = "AppInstance"
+    Name = "jenkins-instance-1"
   }
 }
 
-resource "aws_instance" "docker_instance" {
+resource "aws_instance" "instance_2" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
   key_name      = "dschool"
-
-  vpc_security_group_ids = [
-    "sg-0727ee3d6745a3427",
-  ]
+  vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
 
   tags = {
-    Name = "DockerInstance"
+    Name = "jenkins-instance-2"
   }
 }
 
-output "app_instance_public_ip" {
-  value = aws_instance.app_instance.public_ip
+output "instance_1_ip" {
+  value = aws_instance.instance_1.public_ip
 }
 
-output "docker_instance_public_ip" {
-  value = aws_instance.docker_instance.public_ip
+output "instance_2_ip" {
+  value = aws_instance.instance_2.public_ip
 }
