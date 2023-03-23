3833cb9e20aa6b6e6d28ede69e9f416489481343
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-23 11:05:41 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,26 +2,38 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "java_builder" {
-  ami           = "ami-09cd747c78a9add63"
-  instance_type = "t2.small"
-  key_name      = "dschool"
-
-  vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
-
-  tags = {
-    Name = "java-builder"
-  }
-}
-
 resource "aws_instance" "app_instance" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
   key_name      = "dschool"
 
-  vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
+  vpc_security_group_ids = [
+    "sg-0727ee3d6745a3427",
+  ]
 
   tags = {
-    Name = "app-instance"
+    Name = "AppInstance"
   }
+}
+
+resource "aws_instance" "docker_instance" {
+  ami           = "ami-09cd747c78a9add63"
+  instance_type = "t2.small"
+  key_name      = "dschool"
+
+  vpc_security_group_ids = [
+    "sg-0727ee3d6745a3427",
+  ]
+
+  tags = {
+    Name = "DockerInstance"
+  }
+}
+
+output "app_instance_public_ip" {
+  value = aws_instance.app_instance.public_ip
+}
+
+output "docker_instance_public_ip" {
+  value = aws_instance.docker_instance.public_ip
 }
