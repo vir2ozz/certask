7a3e38fe80f09003f263924787d341034d7aaa3d
Galym <98392003+vir2ozz@users.noreply.github.com>
2023-03-24 11:39:55 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,31 +5,37 @@ provider "aws" {
 resource "aws_instance" "instance1" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
-
-  key_name = "dschool"
-  security_groups = ["launch-wizard-2"]
+  key_name      = "dschool"
+  vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
 
   tags = {
     Name = "instance1"
   }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y openjdk-11-jdk
+              useradd -m -s /bin/bash jenkins
+              echo "jenkins:jenkins" | chpasswd
+              EOF
 }
 
 resource "aws_instance" "instance2" {
   ami           = "ami-09cd747c78a9add63"
   instance_type = "t2.small"
-
-  key_name = "dschool"
-  security_groups = ["launch-wizard-2"]
+  key_name      = "dschool"
+  vpc_security_group_ids = ["sg-0727ee3d6745a3427"]
 
   tags = {
     Name = "instance2"
   }
-}
 
-output "instance1_ip" {
-  value = aws_instance.instance1.public_ip
-}
-
-output "instance2_ip" {
-  value = aws_instance.instance2.public_ip
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y openjdk-11-jdk
+              useradd -m -s /bin/bash jenkins
+              echo "jenkins:jenkins" | chpasswd
+              EOF
 }
